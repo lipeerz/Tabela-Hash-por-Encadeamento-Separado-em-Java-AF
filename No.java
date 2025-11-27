f3a61be5afac63b8be9c844fad26ec8aa061a620
lipeerz <alanfilipe2006@gmail.com>
2025-11-27 16:16:15 -0300

    Update No.java

--- No.java
+++ No.java
@@ -1,1 +1,16 @@
+/*
+Classe simples para o nó da lista encadeada.
+Armazena a chave e a referência para o próximo nó.
+ */
+public class No {
+    public long chave;
+    public No proximo;
 
+    /*
+  Construtor do Nó.
+     */
+    public No(long chave) {
+        this.chave = chave;
+        this.proximo = null;
+    }
+}
